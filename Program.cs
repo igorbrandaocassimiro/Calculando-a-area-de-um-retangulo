73cc28e2b3b837b439465512bb1db770261d945d
Igor Brandão <igorbrandaocassimirodaflon@gmail.com>
2025-08-13 16:56:35 -0300

    Primeiro commit do programa

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] arge)
+    {
+        double largura = 10.5;
+        double comprimento = 20.3;
+
+        double area = largura * comprimento;
+
+       Console.WriteLine($"A área do terreno é: {area} metros quadrados."); 
+
+
+    }
+}
